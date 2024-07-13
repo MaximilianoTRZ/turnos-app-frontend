da7d712859da320aa90d60a17d41b7db18c46917
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2024-07-12 22:36:55 -0300

    frontend working

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Appointment } from "@/components/component/appointment";
+
+export default function Home() {
+  return (
+    <div>
+      <Appointment />
+    </div>
+  );
+}
---
+++ app/turno/page.tsx
@@ -0,0 +1,6 @@
+const page = () => {
+  // se accede con http://localhost:3000/Appointment
+  return <div>pagina para obtener un turno</div>;
+};
+
+export default page;
