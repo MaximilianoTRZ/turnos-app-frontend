5b4c1b8518a08043465e39c45a42587081ba2daa
UdreaLucas <udrealucas@gmail.com>
2024-07-14 14:44:01 -0300

    form new clean looking v1 and in spanish

--- app/(components)/Form.tsx
+++ app/(components)/Form.tsx
@@ -40,121 +40,145 @@ const Form = () => {
     }
   };
   return (
-    <div className="flex w-full justify-center text-center">
+    <div className="flex w-full justify-center items-center min-h-screen bg-gray-100 ">
       <form
         onSubmit={handleSubmit}
         method="POST"
-        className="flex flex-col gap-3 w-1/2"
+        className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md"
       >
-        <h1>Create New Appointment</h1>
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between items-center">
-            <div className="flex flex-col ">
-              <label htmlFor="name">Name</label>
+        <h1 className="text-2xl font-bold mb-6 text-center text-gray-700">
+          Obtener un nuevo turno
+        </h1>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="name" className="text-gray-600">
+                Nombre
+              </label>
               <input
                 id="name"
                 type="text"
                 name="name"
                 onChange={handleChange}
-                required={true}
-                placeholder="Enter your full name"
+                required
+                placeholder="Ingresa tu nombre"
                 value={formData.name}
-                className="m-2 bg-slate-400 p-2 rounded-md "
+                className="mt-2 p-2 border border-gray-300 rounded-md"
               />
             </div>
-            <div className="flex flex-col">
-              <label htmlFor="surname">Surname</label>
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="surname" className="text-gray-600">
+                Apellido
+              </label>
               <input
                 id="surname"
                 type="text"
                 name="surname"
                 onChange={handleChange}
-                required={true}
-                placeholder="Enter your surname"
+                required
+                placeholder="Ingresa tu apellido"
                 value={formData.surname}
-                className="m-2 bg-slate-400 p-2 rounded-md"
+                className="mt-2 p-2 border border-gray-300 rounded-md"
               />
             </div>
           </div>
-          <label htmlFor="dni">DNI</label>
-          <input
-            id="dni"
-            type="text"
-            name="dni"
-            onChange={handleChange}
-            required={true}
-            placeholder="Enter your DNI"
-            value={formData.dni}
-            className="m-2 bg-slate-400 p-2 rounded-md"
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={handleChange}
-            required={true}
-            placeholder="Enter your email"
-            value={formData.email}
-            className="m-2 bg-slate-400 p-2 rounded-md"
-          />
-          <label htmlFor="phone">Phone Number</label>
-          <input
-            id="phone"
-            type="tel"
-            name="phone"
-            onChange={handleChange}
-            required={true}
-            placeholder="Enter your phone number"
-            value={formData.phone}
-            className="m-2 bg-slate-400 p-2 rounded-md"
-          />
-          <div className="flex justify-between items-end">
-            <div className="flex flex-col">
-              <label htmlFor="date">Date</label>
+          <div className="flex flex-col">
+            <label htmlFor="dni" className="text-gray-600">
+              DNI
+            </label>
+            <input
+              id="dni"
+              type="text"
+              name="dni"
+              onChange={handleChange}
+              required
+              placeholder="Ingresa tu DNI"
+              value={formData.dni}
+              className="mt-2 p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-gray-600">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              onChange={handleChange}
+              required
+              placeholder="Ingresa tu email"
+              value={formData.email}
+              className="mt-2 p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="phone" className="text-gray-600">
+              Teléfono
+            </label>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              onChange={handleChange}
+              required
+              placeholder="Ingresa tu número de teléfono"
+              value={formData.phone}
+              className="mt-2 p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div className="flex gap-4">
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="date" className="text-gray-600">
+                Fecha
+              </label>
               <input
                 id="date"
                 type="date"
                 name="date"
                 onChange={handleChange}
-                required={true}
+                required
                 value={formData.date}
-                className="m-2 bg-slate-400 p-2 rounded-md w-full"
+                className="mt-2 p-2 border border-gray-300 rounded-md"
               />
             </div>
-            <div className="flex flex-col">
-              <label htmlFor="time">Time</label>
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="time" className="text-gray-600">
+                Hora
+              </label>
               <input
                 id="time"
                 type="time"
                 name="time"
                 onChange={handleChange}
-                required={true}
+                required
                 value={formData.time}
-                className="m-2 bg-slate-400 p-2 rounded-md w-full"
+                className="mt-2 p-2 border border-gray-300 rounded-md"
               />
             </div>
           </div>
-          <div>
-            <label htmlFor="reason">Reason</label>
+          <div className="flex flex-col">
+            <label htmlFor="reason" className="text-gray-600">
+              Motivo
+            </label>
             <input
               id="reason"
               type="text"
               name="reason"
               onChange={handleChange}
-              required={true}
-              placeholder="Enter the reason for your appointment"
+              required
+              placeholder="Ingresa el motivo de tu consulta"
               value={formData.reason}
-              className="m-2 bg-slate-400 p-2 rounded-md"
+              className="mt-2 p-2 border border-gray-300 rounded-md"
             />
           </div>
+          <input
+            type="submit"
+            value="Obten tu turno"
+            className="mt-6 bg-blue-500 hover:bg-blue-400 text-white py-2 rounded-md transition-colors duration-200"
+          />
+          {error && <p className="text-red-500 mt-4">{error}</p>}
         </div>
-        <input
-          type="submit"
-          value={"Get your Appointment"}
-          className="bg-purple-400 hover:bg-purple-200 text-default-text"
-        />
-        <p className="text-red-500">{error}</p>
       </form>
     </div>
   );
